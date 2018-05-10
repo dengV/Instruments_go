f6628708485552ccf8934673d7b290ae60cf17bf
邓江洲 <dengjingzhou@zto.cn>
2018-05-10 17:02:11 +0800

    iOS Design Patterns 的 3. MVC-N 的挑战者好啦

--- Design_Pattern/RWClean/Clearning Services/Controllers/CleaningServicesTableViewController.swift
+++ Design_Pattern/RWClean/Clearning Services/Controllers/CleaningServicesTableViewController.swift
@@ -4,6 +4,12 @@ import UIKit
 
 public class CleaningServicesTableViewController: UITableViewController {
   
+    private struct SegueIdentifiers{
+        static let business = "Business"
+        static let home = "Home"
+    }   //   新加的
+    
+    
   // MARK: - View Lifecycle
   public override func viewDidLoad() {
     super.viewDidLoad()
@@ -22,4 +28,32 @@ public class CleaningServicesTableViewController: UITableViewController {
   public override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
     return UITableViewAutomaticDimension
   }
+    
+    
+    
+     // MARK: Navi
+    
+    
+    public override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let viewController = segue.destination as? ProductsViewController
+            else {  return  }
+        
+        
+        if segue.identifier == SegueIdentifiers.business {
+            viewController.productControllerOption = .business
+        }
+        else if segue.identifier == SegueIdentifiers.home {
+            viewController.productControllerOption = .home
+        }
+        else{
+            fatalError("Unknown ProductsViewController segue identifier: " + "\(String(describing: segue.identifier))")
+        }
+        
+    }
+    
+    
+    
+    
+    
+    
 }
---
+++ Design_Pattern/RWClean/Clearning Services/Controllers/ProductsViewController.swift
@@ -0,0 +1,146 @@
+//
+//  ProductsViewController.swift
+//  RWClean
+//
+//  Created by dengjiangzhou on 2018/5/10.
+//  Copyright © 2018年 Razeware, LLC. All rights reserved.
+//
+
+import UIKit
+
+class ProductsViewController: UIViewController {
+
+    
+    
+    
+    // MARK: - Injections
+    
+    internal var networkClient = NetworkClient.shared       //  First we get the network client shared instance.
+    
+    //   新加的
+    internal var productControllerOption: Product.ProductType!{
+        didSet{
+            title = productControllerOption.title
+        }
+    }
+/*
+     
+     internal var productType: Product.ProductType! {
+     didSet {
+     title = productType.title
+     }
+     }
+
+     */
+    
+    
+    // MARK: - Instance Properties
+    internal var imageTasks: [IndexPath: URLSessionDataTask] = [:]
+    internal var products: [Product] = []
+    internal let session = URLSession.shared
+    
+    
+    // MARK: - Outlets
+    @IBOutlet internal var collectionView: UICollectionView! {
+        didSet {
+            let refreshControl = UIRefreshControl()
+            refreshControl.addTarget(self, action: #selector(loadProducts), for: .valueChanged)
+            collectionView.refreshControl = refreshControl
+            
+            let layout = collectionView.collectionViewLayout as! UICollectionViewFlowLayout
+            collectionView.collectionViewLayout = CollectionViewCenterFlowLayout(layout: layout)
+        }
+    }
+    
+    
+    
+    internal func loadProducts() {
+        collectionView.refreshControl?.beginRefreshing()
+        
+        networkClient.getProducts(forType: productControllerOption, success: { [weak self] products in
+            guard let strongSelf = self else { return }
+            strongSelf.products = products
+            strongSelf.collectionView.refreshControl?.endRefreshing()
+            strongSelf.collectionView.reloadData()
+        }) { [weak self] error in
+            guard let strongSelf = self else { return }
+            strongSelf.collectionView.refreshControl?.endRefreshing()
+            print("Product download failed: \(error)")
+        }
+        
+    }
+    
+    
+    
+    // MARK: - View Lifecycle
+    public override func viewDidLoad() {
+        super.viewDidLoad()
+        loadProducts()
+    }
+    
+    public override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        guard let selectedItem = collectionView.indexPathsForSelectedItems else { return }
+        selectedItem.forEach{ collectionView.deselectItem(at: $0, animated: false) }
+    }
+    
+    // MARK: - Segue
+    public override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let viewController = segue.destination as? ProductDetailsViewController else { return }
+        let indexPath = collectionView.indexPathsForSelectedItems!.first!
+        let product = products[indexPath.row]
+        viewController.product = product
+    }
+}
+
+
+
+// MARK: - UICollectionViewDataSource
+extension ProductsViewController: UICollectionViewDataSource {
+    
+    public func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return products.count
+    }
+    
+    public func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cellIdentifier = "ProductCell"
+        
+        let product = products[indexPath.row]
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellIdentifier,
+                                                      for: indexPath) as! ProductCollectionViewCell
+        cell.label.text = product.title
+        
+        imageTasks[indexPath]?.cancel()
+        
+        if let url = product.imageURL {
+            let task = session.dataTask(with: url, completionHandler: { [weak cell]
+                (data, response, error) in
+                
+                if let error = error {
+                    print("Image download failed: \(error)")
+                    return
+                }
+                
+                guard let cell = cell,
+                    let data = data,
+                    let image = UIImage(data: data) else {
+                        print("Image download failed: invalid image data!")
+                        return
+                }
+                DispatchQueue.main.async { [weak cell] in
+                    guard let cell = cell else { return }
+                    cell.imageView.image = image
+                }
+            })
+            imageTasks[indexPath] = task
+            task.resume()
+        }
+        return cell
+    }
+}
+
+
+
+//  let url = URL(string: "https://rwcleanbackend.herokuapp.com/products/home")!
+
